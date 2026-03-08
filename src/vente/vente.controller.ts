657d24e5b7c6c77f1d82999404ed78e3bf826b8c
unknown <fanouomega@gmail.com>
2026-03-08 12:55:20 +0300

    feat(vente): operation CRUD

--- src/vente/vente.controller.ts
+++ src/vente/vente.controller.ts
@@ -1,7 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
 import { VenteService } from './vente.service';
+import { CreateVenteDTO } from './dto/create-vente.dto';
+import { UpdateVenteDTO } from './dto/update-vente.dto';
 
 @Controller('vente')
 export class VenteController {
     constructor(private venteService : VenteService){}
+    @Get("")
+    getAll(){
+        return this.venteService.getAll()
+    }
+
+    @Post("")
+    add(@Body(ValidationPipe) body: CreateVenteDTO){
+        return this.venteService.add(body)
+    }
+
+    @Patch(":numProduit")
+    update(@Param("numProduit", ParseIntPipe) numProduit, @Body(ValidationPipe) body : UpdateVenteDTO){
+        return this.venteService.update(numProduit, body)
+    }
+
+    @Delete(":numProduit")
+    remove(@Param("numProduit", ParseIntPipe) numProduit){
+        return this.venteService.remove(numProduit)
+    }
+
+    @Get("stats")
+    getStats(){
+        return this.venteService.stats()
+    }
 }
--- src/vente/vente.service.ts
+++ src/vente/vente.service.ts
@@ -1,7 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateVenteDTO } from './dto/create-vente.dto';
+import { UpdateVenteDTO } from './dto/update-vente.dto';
 
 @Injectable()
 export class VenteService {
     constructor(private prismaService : PrismaService){}
+
+    async getAll(){
+        return await this.prismaService.vente.findMany({orderBy: {numProduit: 'asc'}})
+    }
+
+    async add(data: CreateVenteDTO){
+        return await this.prismaService.vente.create({data})
+    }
+
+    async remove(numProduit:number){
+        return await this.prismaService.vente.delete({where: {numProduit}})
+    }
+
+    async update(numProduit:number, data: UpdateVenteDTO){
+        return await this.prismaService.vente.update({where: {numProduit}, data})
+    }
+
+    async stats(){
+        const ventes =  await this.prismaService.vente.findMany()
+        var montant: number[] = []
+        var montantTotal: number = 0
+        for(let vente of ventes){
+            montantTotal += vente.prix * vente.quantite
+            montant.push(vente.prix * vente.quantite)
+        }
+        var montantFiltre = montant.sort((a, b)=> a - b)
+        return {
+            montantMinimal: montantFiltre[0],
+            montantMaximal: montantFiltre[montantFiltre.length - 1],
+            montantTotal
+        }
+    }
 }
