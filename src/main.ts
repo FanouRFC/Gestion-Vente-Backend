9b28a5e20077fe68c5efb7fe358b4d92d14db1ea
el-oslo <eliada.r.oslo@gmail.com>
2026-06-04 16:38:37 +0300

    refactor(vente): improve code formatting and structure in controller and DTO

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { networkInterfaces } from 'os';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port, '0.0.0.0');
 
   const nets = networkInterfaces();
--- src/vente/dto/create-vente.dto.ts
+++ src/vente/dto/create-vente.dto.ts
@@ -1,6 +1,6 @@
 import { IsNumber, IsString } from "class-validator";
 
-export class CreateVenteDTO{
+export class CreateVenteDTO {
     @IsString()
     design: string
 
